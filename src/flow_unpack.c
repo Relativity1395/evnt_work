7bea9c92fabc9c38e9d9e3256e661305baee7588
Thomas Fletcher <fletchoc1@gmail.com>
2026-08-30 11:56:44 -0400

    cleaned up src

--- src/flow_unpack.c
+++
@@ -1,8 +0,0 @@
-#include "flow_unpack.h"
-
-void flow_unpack(uint64_t word, flow_vector_t *fv) {
-    fv->x  = (uint16_t)((word >> 48) & 0xFFFFu);
-    fv->y  = (uint16_t)((word >> 32) & 0xFFFFu);
-    fv->vx = (int16_t) ((word >> 16) & 0xFFFFu);
-    fv->vy = (int16_t) ((word >>  0) & 0xFFFFu);
-}
