a4c024bc7fa64bdea21b6c1cf1feeb3e32f2a41c
Aaron <aaron.j.altman77@gmail.com>
2026-09-20 01:57:39 -0400

    Fixed Eigen3 library issues

--- src/OpticalFlow.cpp
+++ src/OpticalFlow.cpp
@@ -1,8 +1,8 @@
 #include <../include/OpticalFlow.hpp>
 
-propagatePreviousEvent(const std::vector<Event>& Wi_1, const Eigen::Vector2d& Ui_1, double Ti ){
+std::vector<Eigen::Vector2d> propagatePreviousEvent(const std::vector<Event>& Wi_1, const Eigen::Vector2d& Ui_1, double Ti ){
     std::vector<Eigen::Vector2d> propagatedEvents;
-    for(Event& event : Wi_1){
+    for(const Event& event : Wi_1){
         Eigen::Vector2d x = event.position;
         double t = event.timestamp;
         Eigen::Vector2d propagatedEvent = x + (Ti_1 - t)*Ui_1;
