8da4b6548b0890d930e294ae651e4a36d98857cc
Thomas Fletcher <fletchoc1@gmail.com>
2026-09-25 13:10:52 -0400

    removed bprop from constructor

--- src/OpticalFlow.cpp
+++ src/OpticalFlow.cpp
@@ -16,14 +16,12 @@ Feature::Feature(
     std::vector<Event> currentEvents,
     std::vector<Event> previousEvents,
     Eigen::Vector2d flow,
-    std::vector<Eigen::Vector2d> bProp,
     double xi
 ){
     this->position = position;
     this -> currentEvents = currentEvents;
     this->previousEvents= previousEvents;
     this->flow = flow;
-    this->bProp = bProp;
     this->xi = xi;
     
 }
